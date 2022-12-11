7d180e51fb5804d5f05cfcd3705cc6ef9013e3de
Stéphane Le Solliec <s.lesolliec-external@epexspot.com>
2022-12-11 22:29:29 +0100

    add Tuya

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,8 +1,22 @@
 // we get the room state object and pass it to rfxcom
 
+import { setTargetTemperatures } from "$lib/server/regulator"
 import { start } from "$lib/server/rfxcom"
+import { getAllData } from "$lib/server/tuya"
 
 console.log('app server starting')
 
+// RFXcom
 start()
 
+/*
+// Tuya
+getAllData()
+
+let tuyaInterval
+if (! tuyaInterval) {
+  setInterval(getAllData, 300 * 1000)
+}
+*/
+
+setTargetTemperatures()
