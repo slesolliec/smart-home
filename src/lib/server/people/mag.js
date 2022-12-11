559491c0a3f3bcc50b3d6e484ccfd1d21c8d0378
Stéphane Le Solliec <s.lesolliec-external@epexspot.com>
2022-12-11 22:29:56 +0100

    add people and regulator

---
+++ src/lib/server/people/mag.js
@@ -0,0 +1,23 @@
+
+const mag = {
+  teletravail: {
+    name: 'Télétravail',
+    color: 'orange',
+    rooms: [
+      {
+        name: 'Chambre',
+        hours: { '0h00': 15, '5h30': 17, '7h45': 12, '16h00': 15, '18h00': 17, '21h00': 15 }
+      },
+      {
+        name: 'Cuisine',
+        hours: { '6h00': 18, '7h45': 12, '11h30': 18, '13h45': 12, '16h00': 18, '21h00': 12 }
+      },
+      {
+        name: 'Mag',
+        hours: { '7h30': 20, '17h30': 5 }
+      },
+    ]
+  }
+}
+
+export default mag
