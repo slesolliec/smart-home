8f0cb38211b341f1fe8743b42d49609a9b27f9bc
Stéphane Le Solliec <s.lesolliec-external@epexspot.com>
2022-12-12 19:04:59 +0100

    add week to GUI

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,9 +1,10 @@
-import rooms from "$lib/server/rooms"
-
+import rooms  from "$lib/server/rooms"
+import people from "$lib/server/people"
+import week   from "$lib/server/week"
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
 
-  return { rooms }
+  return { rooms, people, week }
 }
 
