35ee07f2aa804ae4f144781d03a70a505360b744
Stéphane Le Solliec <s.lesolliec-external@epexspot.com>
2022-12-12 19:03:57 +0100

    add week and regulator

---
+++ src/lib/server/week.js
@@ -0,0 +1,18 @@
+
+import { mag, lili } from "$lib/server/people"
+
+const week = [
+  // dimanche
+  { mag: 'repos', lili: 'repos' },
+  // lundi
+  { mag: 'teletravail', lili: 'ecole' },
+  { mag: 'repos',       lili: 'ecole' },
+  { mag: 'repos',       lili: 'repos' },
+  { mag: 'angers',      lili: 'ecole' },
+  { mag: 'angers',      lili: 'ecole' },
+  { mag: 'repos',       lili: 'repos' },
+
+]
+
+
+export default week
