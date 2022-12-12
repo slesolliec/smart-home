82a03dfad9dde4a1b6dff562aa7bbbdea02a5541
Stéphane Le Solliec <s.lesolliec-external@epexspot.com>
2022-12-12 19:58:07 +0100

    upgrade people + stef

--- src/lib/server/people.js
+++ src/lib/server/people.js
@@ -1,7 +1,8 @@
 
-import mag from "$lib/server/people/mag"
+import mag  from "$lib/server/people/mag"
 import lili from "$lib/server/people/lili"
+import stef from "$lib/server/people/stef"
 
-const people = [mag, lili]
+const people = [mag, lili, stef]
 
 export default people
