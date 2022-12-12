672e9b15310b688ff263bdff07fd39f6697b49ce
Stéphane Le Solliec <s.lesolliec-external@epexspot.com>
2022-12-12 19:18:21 +0100

    update rooms

--- src/lib/server/rooms.js
+++ src/lib/server/rooms.js
@@ -1,16 +1,16 @@
 // this is the room state of our application
 
 const rooms = [
-  { name: 'Matys',   sensor: '0xFF01', power: '0',  tempTarget: 12,  tempCurrent: 10,  },
-  { name: 'Palier',  sensor: '0x7801', power: '0',  tempTarget: 12,  tempCurrent: 10,  },
-  { name: 'SdBainH', sensor: '0xE001', power: '0',  tempTarget: 12,  tempCurrent: 10,  },
-  { name: 'Lili',    sensor: '0x3D01', power: '0',  tempTarget: 12,  tempCurrent: 10,  },
-  { name: 'Cuisine', sensor: '0xD101', power: '0',  tempTarget: 12,  tempCurrent: 10,  },
-  { name: 'Salon',   sensor: '0xB002', power: '0',  tempTarget: 12,  tempCurrent: 10,  },
-  { name: 'SdBain',  sensor: '0xFE01', power: '0',  tempTarget: 12,  tempCurrent: 10,  },
-  { name: 'Chambre', sensor: '0xA00E', power: '0',  tempTarget: 12,  tempCurrent: 10,  },
-  { name: 'Mag',     sensor: '',       power: '0',  tempTarget: 18,  tempCurrent: 10,  },
-  { name: 'Stef',    sensor: '0x9101', power: '0',  tempTarget: 15,  tempCurrent: 10,  },
+  { name: 'Matys',   sensor: '0xFF01', power: '0',  tempTarget: 12,  tempCurrent: 10,  tuyaId: 'bfb7fd9135eb2dd73egztw' },
+  { name: 'Palier',  sensor: '0x7801', power: '0',  tempTarget: 12,  tempCurrent: 10,  tuyaId: '' },
+  { name: 'SdBainH', sensor: '0xE001', power: '0',  tempTarget: 12,  tempCurrent: 10,  tuyaId: '' },
+  { name: 'Lili',    sensor: '0x3D01', power: '0',  tempTarget: 12,  tempCurrent: 10,  tuyaId: '' },
+  { name: 'Cuisine', sensor: '0xD101', power: '0',  tempTarget: 12,  tempCurrent: 10,  tuyaId: 'bf7d504a06f0e24223ezr3' },
+  { name: 'Salon',   sensor: '0xB002', power: '0',  tempTarget: 12,  tempCurrent: 10,  tuyaId: '' },
+  { name: 'SdBain',  sensor: '0xFE01', power: '0',  tempTarget: 12,  tempCurrent: 10,  tuyaId: '' },
+  { name: 'Chambre', sensor: '0xA00E', power: '0',  tempTarget: 12,  tempCurrent: 10,  tuyaId: '' },
+  { name: 'Mag',     sensor: '',       power: '0',  tempTarget: 5,   tempCurrent: 10,  tuyaId: 'bf62c42f1b30f7540boel6' },
+  { name: 'Stef',    sensor: '0x9101', power: '0',  tempTarget: 5,   tempCurrent: 10,  tuyaId: 'bf07ef88e08e910889gv2l' },
 ]
 
 // '0x200E': { name: 'Cellier' },
