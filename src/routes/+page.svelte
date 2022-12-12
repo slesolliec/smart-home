38a5279d05b63644b9192e299ff7a00db1905a7a
Stéphane Le Solliec <s.lesolliec-external@epexspot.com>
2022-12-12 19:04:59 +0100

    add week to GUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,15 @@ import Room from "$lib/components/Room.svelte"
 /** @type {import('./$types').PageLoad} */
 export let data
 
-let rooms = data.rooms
+let rooms  = data.rooms
+let people = data.people
+let week   = data.week
 
+let currentDay = new Date().getDay()
+
+const days = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi']
 
 </script>
-
-
 
 <div class="house">
   {#each rooms as room}
@@ -19,4 +22,20 @@ let rooms = data.rooms
   {/each}
 </div>
 
+<div class="people">
+  <div></div>
+  {#each [1, 2, 3, 4, 5, 6, 0] as day}
+    <div class={currentDay == day ? ' today' : ''}>
+      <span class="small-day">{days[day].slice(0, 3)}.</span><span class="long-day">{days[day]}</span>
+    </div>
+  {/each}
 
+  {#each people as person}
+    <div class="bold">{person.name}</div>
+    {#each [1, 2, 3, 4, 5, 6, 0] as day}
+      <div class={'activity ' + week[day][person.name.toLowerCase()] + (currentDay == day ? ' today' : '')}>
+        {week[day][person.name.toLowerCase()]}
+      </div>
+    {/each}
+  {/each}
+</div>
