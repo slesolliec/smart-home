8d0abf827e201cea1ec2ae2925c2bd92d3770088
Stéphane Le Solliec <s.lesolliec-external@epexspot.com>
2022-12-12 19:13:10 +0100

    add tuya-keys

---
+++ src/lib/server/tuya-keys.js
@@ -0,0 +1,13 @@
+
+const keys = {
+  'bf07ef88e08e910889gv2l': '',
+  'bf62c42f1b30f7540boel6': '',
+  'bf7d504a06f0e24223ezr3': '',
+  'bfb7fd9135eb2dd73egztw': '',
+}
+
+function getTuyaKey(id = '') {
+  return keys[id]
+}
+
+export default getTuyaKey
