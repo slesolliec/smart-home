dad99ceba591d71486c4c16e8e74a7bdf76dcdf0
Stéphane Le Solliec <s.lesolliec-external@epexspot.com>
2022-12-12 19:02:53 +0100

    update people

---
+++ src/lib/server/people/lili.js
@@ -0,0 +1,22 @@
+
+const lili = {
+  name: 'Lili',
+  ecole: {
+    name: 'Ecole',
+    color: 'orange',
+    rooms: [
+      { name: 'Lili',     hours: { '0h00': 15, '5h30': 17, '7h45': 12, '16h00': 18, '21h00': 15 }},
+      { name: 'SdeBainH', hours: { '0h00': 13, '5h30': 17, '7h45': 12, '16h00': 17, '21h00': 13 }},
+    ]
+  },
+  repos: {
+    name: 'Repos',
+    color: 'green',
+    rooms: [
+      { name: 'Lili',     hours: { '0h00': 15, '9h00': 17, '10h00': 18, '21h00': 15 }},
+      { name: 'SdeBainH', hours: { '0h00': 13, '8h00': 15, '10h00': 17, '21h00': 13 }},
+    ]
+  },
+}
+
+export default lili
--- src/lib/server/people/mag.js
+++ src/lib/server/people/mag.js
@@ -1,23 +1,32 @@
 
 const mag = {
+  name: 'Mag',
   teletravail: {
     name: 'Télétravail',
     color: 'orange',
     rooms: [
-      {
-        name: 'Chambre',
-        hours: { '0h00': 15, '5h30': 17, '7h45': 12, '16h00': 15, '18h00': 17, '21h00': 15 }
-      },
-      {
-        name: 'Cuisine',
-        hours: { '6h00': 18, '7h45': 12, '11h30': 18, '13h45': 12, '16h00': 18, '21h00': 12 }
-      },
-      {
-        name: 'Mag',
-        hours: { '7h30': 20, '17h30': 5 }
-      },
+      { name: 'Chambre', hours: { '0h00': 15, '5h30': 17, '7h45': 12, '16h00': 15, '18h00': 17, '21h00': 15 }},
+      { name: 'Cuisine', hours: { '6h00': 18, '7h45': 12, '11h30': 18, '13h45': 12, '16h00': 18, '21h00': 12 }},
+      { name: 'Mag',     hours: { '7h30': 20, '17h30': 5 }},
     ]
-  }
+  },
+  angers: {
+    name: 'Angers',
+    color: 'red',
+    rooms: [
+      { name: 'Chambre', hours: { '0h00': 15, '5h30': 17, '7h45': 12, '16h00': 15, '18h00': 17, '21h00': 15 }},
+      { name: 'Cuisine', hours: { '6h00': 18, '7h45': 12, '16h00': 18, '21h00': 12 }},
+    ]
+  },
+  repos: {
+    name: 'Repos',
+    color: 'green',
+    rooms: [
+      { name: 'Chambre', hours: { '0h00': 15, '10h00': 17, '12h00': 15, '18h00': 17, '21h00': 15 }},
+      { name: 'Cuisine', hours: { '9h00': 17, '18h00': 18, '21h00': 12 }},
+      { name: 'Salon',   hours: { '9h00': 17, '18h00': 18, '21h00': 12 }},
+    ]
+  },
 }
 
 export default mag
