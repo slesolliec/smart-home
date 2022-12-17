8292038a1d671d79b20e4d3f0366990d353ff907
slesolliec <slesolliec@gmail.com>
2022-12-17 15:19:09 +0100

    add Matys

--- src/lib/server/week.js
+++ src/lib/server/week.js
@@ -2,14 +2,14 @@
 
 const week = [
   // dimanche
-  { maison: 'base', mag: 'repos',       lili: 'repos',  stef: 'repos' },
+  { maison: 'base', mag: 'repos',       lili: 'repos',  stef: 'repos', matys: 'repos'},
   // lundi
-  { maison: 'base', mag: 'teletravail', lili: 'ecole',  stef: 'matys' },
-  { maison: 'base', mag: 'repos',       lili: 'ecole',  stef: 'matys' },
-  { maison: 'base', mag: 'repos',       lili: 'repos',  stef: 'matys' },
-  { maison: 'base', mag: 'angers',      lili: 'ecole',  stef: 'matys' },
-  { maison: 'base', mag: 'angers',      lili: 'ecole',  stef: 'matys' },
-  { maison: 'base', mag: 'repos',       lili: 'repos',  stef: 'repos' },
+  { maison: 'base', mag: 'teletravail', lili: 'ecole',  stef: 'matys', matys: 'repos' },
+  { maison: 'base', mag: 'repos',       lili: 'ecole',  stef: 'matys', matys: 'repos' },
+  { maison: 'base', mag: 'repos',       lili: 'repos',  stef: 'matys', matys: 'repos' },
+  { maison: 'base', mag: 'angers',      lili: 'ecole',  stef: 'matys', matys: 'repos' },
+  { maison: 'base', mag: 'angers',      lili: 'ecole',  stef: 'matys', matys: 'repos' },
+  { maison: 'base', mag: 'repos',       lili: 'repos',  stef: 'repos', matys: 'repos' },
 
 ]
 
