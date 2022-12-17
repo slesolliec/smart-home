020a7ef7b48903813cb7e922e9e79d893eec28cb
slesolliec <slesolliec@gmail.com>
2022-12-17 15:04:21 +0100

    add new smart plugs to rooms

--- src/lib/server/rooms.js
+++ src/lib/server/rooms.js
@@ -3,14 +3,14 @@
 const rooms = [
   { name: 'Matys',   sensor: '0xFF01', power: '0', switchOn: false, isSwitch: true,  tempTarget: 12,  tempCurrent: 10,  tuyaId: 'bfb7fd9135eb2dd73egztw' },
   { name: 'Palier',  sensor: '0x7801', power: '0', switchOn: false, isSwitch: false, tempTarget: 12,  tempCurrent: 10,  tuyaId: '' },
-  { name: 'SdBainH', sensor: '0xE001', power: '0', switchOn: false, isSwitch: false, tempTarget: 12,  tempCurrent: 10,  tuyaId: '' },
+  { name: 'SdBainH', sensor: '0xE001', power: '0', switchOn: false, isSwitch: true,  tempTarget: 12,  tempCurrent: 10,  tuyaId: 'bf127e02e7ea1f91bcjehc' },
   { name: 'Lili',    sensor: '0x3D01', power: '0', switchOn: false, isSwitch: false, tempTarget: 12,  tempCurrent: 10,  tuyaId: '' },
   { name: 'Cuisine', sensor: '0xD101', power: '0', switchOn: false, isSwitch: true,  tempTarget: 12,  tempCurrent: 10,  tuyaId: 'bf7d504a06f0e24223ezr3' },
   { name: 'Salon',   sensor: '0xB002', power: '0', switchOn: false, isSwitch: false, tempTarget: 12,  tempCurrent: 10,  tuyaId: '' },
-  { name: 'SdBain',  sensor: '0xFE01', power: '0', switchOn: false, isSwitch: true,  tempTarget: 12,  tempCurrent: 10,  tuyaId: 'bf127e02e7ea1f91bcjehc' },
-  { name: 'Chambre', sensor: '0xA00E', power: '0', switchOn: false, isSwitch: false, tempTarget: 12,  tempCurrent: 10,  tuyaId: '' },
+  { name: 'SdBain',  sensor: '0xFE01', power: '0', switchOn: false, isSwitch: true,  tempTarget: 12,  tempCurrent: 10,  tuyaId: 'bf14500a4d6f71a2b0l8zr' },
+  { name: 'Chambre', sensor: '0xA00E', power: '0', switchOn: false, isSwitch: true,  tempTarget: 12,  tempCurrent: 10,  tuyaId: 'bf07ef88e08e910889gv2l' },
   { name: 'Mag',     sensor: '',       power: '0', switchOn: false, isSwitch: false, tempTarget: 5,   tempCurrent: 10,  tuyaId: '' },  // 'bf62c42f1b30f7540boel6'
-  { name: 'Stef',    sensor: '0x9101', power: '0', switchOn: false, isSwitch: false, tempTarget: 5,   tempCurrent: 10,  tuyaId: 'bf07ef88e08e910889gv2l' },
+  { name: 'Stef',    sensor: '0x9101', tuyaSensor: 'bfd7e19d42b0b3539axjxw', power: '0', switchOn: false, isSwitch: false, tempTarget: 5,   tempCurrent: 10,  tuyaId: '' },
 ]
 
 // '0x200E': { name: 'Cellier' },
