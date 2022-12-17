0072a9146184ac7fb166e5a03825040f398fbe0f
slesolliec <slesolliec@gmail.com>
2022-12-17 15:59:04 +0100

    hide power when no smartplug

--- src/lib/components/Room.svelte
+++ src/lib/components/Room.svelte
@@ -1,11 +1,12 @@
 <script>
 
 export let room = {
-  name: "my room",
-  power: 0,
-  switchOn: false,
-  tempTarget: 15,
-  tempCurrent: 10
+  name:        "my room",
+  power:       0,
+  switchOn:    false,
+  tempTarget:  15,
+  tempCurrent: 10,
+  tuyaId:      ''
 }
 
 </script>
@@ -14,7 +15,9 @@ export let room = {
 
 
 <div class="room">
-  <div class={room.switchOn ? 'power on' : 'power off' }>{room.power}W</div>
+  {#if room.tuyaId != ''}
+    <div class={room.switchOn ? 'power on' : 'power off' }>{room.power}W</div>
+  {/if}
   <div class="temp-target">{room.tempTarget}°</div>
   <div class="temp-current">{room.tempCurrent}°</div>
   <div class="name">{room.name}</div>
