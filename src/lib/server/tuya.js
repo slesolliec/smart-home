f81709e17acbf8e4c21e7b68cce45e09488ac068
slesolliec <slesolliec@gmail.com>
2022-12-23 00:04:10 +0100

    store smart plug data

--- src/lib/server/tuya.js
+++ src/lib/server/tuya.js
@@ -4,39 +4,49 @@ import TuyAPI        from 'tuyapi'
 import getTuyaKey    from "$lib/server/tuya-keys"
 import log           from "$lib/server/log"
 import { minString } from "$lib/utils"
+import { Room, SmartPlug } from "./db"
+import { Op }        from 'sequelize'
 
 
 async function getAllData() {
-  for (const i in rooms) {
-    let room = rooms[i]
-    if (room.tuyaId) {
-      const device = new TuyAPI({id: room.tuyaId, key: getTuyaKey(room.tuyaId)})
-      try {
-        await device.find()
-        await device.connect()
-        const {dps} = await device.get({schema: true})
-        await device.disconnect()
 
-        room.power = Math.floor(dps['19'] / 10)
-        room.switchOn = dps['1']
-        
-        let msg = minString(room.name, 9, true)
-        msg += dps['1'] ? 'On  ' : 'Off '
-        msg += minString(dps['17'], 3, false) + ' '
-        msg += minString(dps['18'], 4, false) + ' mA  '
-        msg += minString(Math.floor(dps['19'] / 10), 4, false) + ' W  '
-        msg += minString(Math.floor(dps['20'] / 10), 3, false) + ' V  '
-        msg += dps['22'] + '  '
-        msg += dps['23'] + '  '
-        msg += dps['24'] + '  '
-        msg += dps['25'] + '  '
+  const rooms = await Room.findAll({where: { smart_plug: {[Op.not]: ''}}})
+
+  for (const room of rooms) {
+
+    const device = new TuyAPI({id: room.smart_plug, key: getTuyaKey(room.smart_plug)})
+    try {
+      await device.find()
+      await device.connect()
+      const {dps} = await device.get({schema: true})
+      await device.disconnect()
+
+      SmartPlug.create({
+        room_id: room.room_id,
+        is_on: dps['1'],
+        power: Math.floor(dps['19'] / 10),
+        dps_22: dps['22'],
+        dps_23: dps['23'],
+        dps_24: dps['24'],
+        dps_25: dps['25'],
+      })
+
+      let msg = minString(room.name, 9, true)
+      msg += dps['1'] ? 'On  ' : 'Off '
+      msg += minString(dps['17'], 3, false) + ' '
+      msg += minString(dps['18'], 4, false) + ' mA  '
+      msg += minString(Math.floor(dps['19'] / 10), 4, false) + ' W  '
+      msg += minString(Math.floor(dps['20'] / 10), 3, false) + ' V  '
+      msg += dps['22'] + '  '
+      msg += dps['23'] + '  '
+      msg += dps['24'] + '  '
+      msg += dps['25'] + '  '
+
+      log.tuya(msg)
   
-        log.tuya(msg)
-  
-      } catch (err) {
-        log.tuya("Failed getting state in " + room.name + ": " + err)
-        console.log(err)
-      }
+    } catch (err) {
+      log.tuya("Failed getting state in " + room.name + ": " + err)
+      console.log(err)
     }
 
     /*
