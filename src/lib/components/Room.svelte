6426c1be4bb231b782e23265371d5a5aadb0a949
Stéphane Le Solliec <s.lesolliec-external@epexspot.com>
2022-12-23 23:46:14 +0100

    hide target temp when no smart plug

--- src/lib/components/Room.svelte
+++ src/lib/components/Room.svelte
@@ -18,7 +18,9 @@ export let room = {
   {#if room.smart_plug}
     <div class={room.is_on ? 'power on' : 'power off' }>{room.power}W</div>
   {/if}
+  {#if room.smart_plug}
   <div class="temp-target">{room.tempTarget}°</div>
+  {/if}
   <div class="temp-current">
     {#if room.temp}
       {String(room.temp).slice(0, -1)}<small>.{String(room.temp).slice(-1)}</small>°
